931364dadab27661a9be01996f730ea8c1199c77
blaine12100 <marwha.dhruv@gmail.com>
2019-02-16 02:02:48 +0530

    Another Missed File

---
+++ Data Structures Track/ReverseaLinkedList.py
@@ -0,0 +1,80 @@
+'''Reverse a Linked List.This is the cheapest solution possible.There is a way to do
+this natively.Have to figure it out'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+# Complete the reversePrint function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+
+def reversePrint(head):
+    base_element = head
+
+    rev_list = []
+
+    while base_element != None:
+        rev_list.append(base_element.data)
+        base_element = base_element.next
+
+    print("\n".join(str(e) for e in rev_list[::-1]))
+
+
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
